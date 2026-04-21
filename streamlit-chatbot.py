7b5088768fd03b707e38c751dbf339d855c37562
likeKHW <likegundam@gmail.com>
2026-04-21 10:36:33 +0900

    Add files via upload

--- streamlit-chatbot.py
+++ streamlit-chatbot.py
@@ -2,9 +2,7 @@ from openai import OpenAI
 import streamlit as st
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 with st.sidebar:
     # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key")
@@ -12,10 +10,8 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-
-st.title("💬 Chatbot ^^")
+st.title("💬 Chatbot ^___^")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
-
 
 # 대화 내용을 저장할 변수를 초기화
 if "messages" not in st.session_state:
@@ -23,11 +19,9 @@ if "messages" not in st.session_state:
         {"role": "system", "content": "How can I help you?"},
     ]
 
-
 # 대화 내용을 화면에 출력
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-
 
 # 사용자 입력을 대기
 if prompt := st.chat_input():
@@ -35,10 +29,8 @@ if prompt := st.chat_input():
     #     st.info("Please add your OpenAI API key to continue.")
     #     st.stop()
 
-
     # client = OpenAI(api_key=openai_api_key)
     client = OpenAI()
-
 
     # 사용자 입력을 대화 내용에 추가하고 화면에 출력
     st.session_state.messages.append(
@@ -46,17 +38,14 @@ if prompt := st.chat_input():
     )
     st.chat_message("user").write(prompt)
 
-
     # LLM 호출
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=st.session_state.messages
+    response = client.responses.create(
+        model="gpt-3.5-turbo", 
+        input=st.session_state.messages
     )
 
-
     # LLM 응답에서 텍스트를 추출
-    msg = response.choices[0].message.content
-
+    msg = response.output_text
 
     # LLM 응답을 대화 내용에 추가하고 화면에 출력
     st.session_state.messages.append(
